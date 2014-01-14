edccc47ea7bec4573964ff5ea3c1d11264e23be8
Avery Rozar <avery.rozar@insecure-it.com>
2014-01-14 16:02:15 -0500

    Working on --host_file

--- asaos-snmpv3-tool-hostfile.py
+++ asaos-snmpv3-tool-hostfile.py
@@ -64,37 +64,10 @@ def connect(user, host, passwd, en_passwd):
     child.expect(PROMPT)
     child.sendline('config t')
     child.expect(PROMPT)
-
     return child
-
-def connects(user, hosts, passwd, en_passwd):
-    ssh_newkey = 'Are you sure you want to continue connecting?'
-    constr = 'ssh ' + user + '@' + hosts
-    child = pexpect.spawn(constr)
-    ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword:'])
-
-    if ret == 0:
-        print '[-] Error Connecting'
-        return
-    if ret == 1:
-        child.sendline('yes')
-        ret = child.expect([pexpect.TIMEOUT, '[P|p]assword:'])
-        if ret == 0:
-            print '[-] Error Connecting'
-            return
-    child.sendline(passwd)
-    child.expect(PROMPT)
-    child.sendline('enable')
-    child.sendline(en_passwd)
-    child.expect(PROMPT)
-    child.sendline('config t')
-    child.expect(PROMPT)
-
-    return child
-
 
 def main():
-    parser = argparse.ArgumentParser('usage %prog ' + '--host --host_file --username --password --enable --group --snmp_user --snmp_host --int_name --snmp_v3_auth --snmp_v3_hmac --snmp_v3_priv --snmp_v3_encr')
+    parser = argparse.ArgumentParser('usage %prog ' + '--host --host_file --username --password--enable --group --snmp_user --snmp_host --int_name --snmp_v3_auth --snmp_v3_hmac --snmp_v3_priv --snmp_v3_encr')
     parser.add_argument('--host', dest='host', type=str, help='specify a target host')
     parser.add_argument('--host_file', dest='hfile', type=str, help='specify a target host file')
     parser.add_argument('--username', dest='user', type=str, help='specify a user name')
@@ -122,29 +95,27 @@ def main():
     snmpauth = args.snmpauth
     snmppriv = args.snmppriv
     snmpencrypt = args.snmpencrypt
-    hosts = open(hfile, 'r')
 
-# This part is not working yet!
-    for line in hosts.readlines():
+    if hfile:
+        with open(hfile) as hosts:
+            for line in hosts:
+                host = line.rstrip
+                child = connect(user, host, passwd, en_passwd)
+                send_command(child, SNMPGROUPCMD + group + V3PRIVCMD)
+                send_command(child, SNMPSRVUSRCMD + snmpuser + ' ' + group + V3AUTHCMD + SHAHMACCMD + snmpauth + PRIVCMD + snmpencrypt + ' ' + snmppriv)
+                send_command(child, SNMPSRVHOSTCMD + intname + ' ' + snmphost + VERSION3CMD + snmpuser)
+                send_command(child, SNMPSRVENTRAP)
+                send_command(child, WRME)
 
-        if hfile == True:
-            hosts = line.strip('r').strip('\n')
-            child = connects(user, hosts, passwd, en_passwd)
-            send_command(child, SNMPGROUPCMD + group + V3PRIVCMD)
-            send_command(child, SNMPSRVUSRCMD + snmpuser + ' ' + group + V3AUTHCMD + SHAHMACCMD + snmpauth + PRIVCMD + snmpencrypt + ' ' + snmppriv)
-            send_command(child, SNMPSRVHOSTCMD + intname + ' ' + snmphost + VERSION3CMD + snmpuser)
-            send_command(child, SNMPSRVENTRAP)
-            send_command(child, WRME)
-            exit()
-# Wish it was, I'm working on it
-
-    child = connect(user, host, passwd, en_passwd)
-    send_command(child, SNMPGROUPCMD + group + V3PRIVCMD)
-    send_command(child, SNMPSRVUSRCMD + snmpuser + ' ' + group + V3AUTHCMD + SHAHMACCMD + snmpauth + PRIVCMD + snmpencrypt + ' ' + snmppriv)
-    send_command(child, SNMPSRVHOSTCMD + intname + ' ' + snmphost + VERSION3CMD + snmpuser)
-    send_command(child, SNMPSRVENTRAP)
-    send_command(child, WRME)
-    exit()
+    elif host:
+        child = connect(user, host, passwd, en_passwd)
+        send_command(child, SNMPGROUPCMD + group + V3PRIVCMD)
+        send_command(child, SNMPSRVUSRCMD + snmpuser + ' ' + group + V3AUTHCMD + SHAHMACCMD + snmpauth + PRIVCMD + snmpencrypt + ' ' + snmppriv)
+        send_command(child, SNMPSRVHOSTCMD + intname + ' ' + snmphost + VERSION3CMD + snmpuser)
+        send_command(child, SNMPSRVENTRAP)
+        send_command(child, WRME)
+    else:
+        print ('Specify euther --host or --host_file or I have nothing to do')
 
 if __name__ == '__main__':
     main()
