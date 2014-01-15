6398261936794735116a4c779888a4f1e79ca62b
Avery Rozar <avery.rozar@insecure-it.com>
2014-01-14 19:14:11 -0500

    Working on --host_file

--- asaos-snmpv3-tool.py
+++ asaos-snmpv3-tool.py
@@ -121,7 +121,7 @@ def main():
     snmpencrypt = args.snmpencrypt
 
     if hosts:
-        with open(hosts, mode='r', buffering=1):
+        with open(hosts, mode='r', buffering=2):
             for line in hosts:
                 hosts = line.rstrip
                 child = connects(user, hosts, passwd, en_passwd)
