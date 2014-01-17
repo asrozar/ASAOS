46d848b9d5f5b7ec82be74460d742bbb4362e74e
Avery Rozar <avery.rozar@insecure-it.com>
2014-01-17 08:29:45 -0500

    clean up

--- asaos-snmpv3-tool.py
+++ asaos-snmpv3-tool.py
@@ -71,8 +71,8 @@ def connect(user, host, passwd, en_passwd):
 
 
 def main():
-    parser = argparse.ArgumentParser('usage %prog ' + '--host --host_file --username --password--enable --group\
-    --snmp_user --snmp_host --snmp_contact --int_name --snmp_v3_auth --snmp_v3_hmac --snmp_v3_priv --snmp_v3_encr')
+    parser = argparse.ArgumentParser('--host --host_file --username --password--enable --group --snmp_user --snmp_host\
+    --snmp_contact --int_name --snmp_v3_auth --snmp_v3_hmac --snmp_v3_priv --snmp_v3_encr')
     parser.add_argument('--host', dest='host', type=str, help='specify a target host')
     parser.add_argument('--host_file', dest='hosts', type=file, help='specify a target host file')
     parser.add_argument('--username', dest='user', type=str, help='specify a user name')
