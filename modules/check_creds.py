5f07625b4cd3b25e99844f8e3e2d86e8b51fb917
Avery Rozar <avery.rozar@insecure-it.com>
2014-02-09 15:55:49 -0500

    building passwd-check module

---
+++ modules/check_creds.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""          ...
+        .:::|#:#|::::.
+     .:::::|##|##|::::::.
+     .::::|##|:|##|:::::.
+      ::::|#|:::|#|:::::
+      ::::|#|:::|#|:::::
+      ::::|##|:|##|:::::
+      ::::.|#|:|#|.:::::
+      ::|####|::|####|::
+      :|###|:|##|:|###|:
+      |###|::|##|::|###|
+      |#|::|##||##|::|#|
+      |#|:|##|::|##|:|#|
+      |#|##|::::::|##|#|
+       |#|::::::::::|#|
+        ::::::::::::::
+          ::::::::::
+           ::::::::
+            ::::::
+              ::
+"""
+__author__ = 'Avery Rozar'
+
+import pexpect
+import getpass
+import argparse
+
+
+def check_creds(host, user, passwd, en_passwd):
+
+    ssh_newkey = 'Are you sure you want to continue connecting (yes/no)?'
+    constr = 'ssh ' + user + '@' + host
+    ssh = pexpect.spawn(constr)
+    ret = ssh.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword:'])
+    if ret == 0:
+        print '[-] Error Connecting to ' + host
+        return
+    if ret == 1:
+        ssh.sendline('yes')
+        ret = ssh.expect([pexpect.TIMEOUT, '[P|p]assword:'])
+        if ret == 0:
+            print '[-] Could not accept new key from ' + host
+            return
+    ssh.sendline(passwd)
+    auth = ssh.expect(['[P|p]assword:', '.>', '.#'])
+    if auth == 0:
+        print 'User password is incorrect'
+        return
+    if auth == 1:
+        print('login is correct')
+        ssh.sendline('enable')
+        ssh.sendline(en_passwd)
+        enable = ssh.expect([pexpect.TIMEOUT, '.#'])
+        if enable == 0:
+            print 'enable password is incorrect'
+            return
+        if enable == 1:
+            print 'enable password is correct'
+            return
+    if auth == 2:
+        print 'privilege mode creds work'
+        return
+    else:
+        print 'creds are incorrect'
+        return
+
+def main():
+    parser = argparse.ArgumentParser('--host --user --passwd --en_passwd')
+    parser.add_argument('--host', dest='host', type=str)
+    parser.add_argument('--user', dest='user', type=str)
+    parser.add_argument('--passwd', dest='passwd', type=str)
+    parser.add_argument('--en_passwd', dest='en_passwd', type=str)
+
+    args = parser.parse_args()
+    host = args.host
+    user = args.user
+    passwd = args.passwd
+    en_passwd = args.en_passwd
+
+    if host is None:
+        host = raw_input('host: ')
+    if user is None:
+        user = raw_input('user: ')
+    if passwd is None:
+        passwd = getpass.getpass('passwd: ')
+    if en_passwd is None:
+        en_passwd = getpass.getpass('en_passwd: ')
+    check_creds(host, user, passwd, en_passwd)
+
+if __name__ == '__main__':
+    main()
